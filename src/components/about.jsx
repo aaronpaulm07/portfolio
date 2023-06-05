238087758fea3cf603ac55fe451c0552896b4b58
aaronpaulm07 <aaronpaulm07@yahoo.com>
2023-06-05 10:27:02 +0400

    ABOUT CHNAGEDDD

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -7,19 +7,15 @@ const about = () => {
         <div className='abt' id='abt'>
             <h1 className='aboutH'>ABOUT</h1>
             <h2 className='name'>AARON PAUL</h2>
-            <p1 className='para'>I'm a computer science engineering student pursuing my Bachelor's 
-            degree from Cochin University. With a passion for technology, I have completed an internship in web development, 
-            where I worked with HTML, CSS, C/C++, and JavaScript. I am also proficient in React and have completed a course on 
-            Python data analysis from LinkedIn Learning.
-            <br/>
-            While I enjoy the technical aspects of programming, my ultimate goal is to explore the creative side of technology. 
-            I am particularly interested in UI/UX design and analysis, where I can use my technical skills to create engaging 
-            and user-friendly interfaces.
-            <br/>
-            <br/>
-            Aside from my academic pursuits, I am a highly ambitious and organized individual who is always seeking new challenges 
-            and opportunities to grow. I have a passion for traveling and exploring new places, which has broadened my horizons and 
-            helped me develop a unique perspective.</p1>
+            <p1 className='para'>As a recent graduate, I am highly interested in the fields of data analytics, UI/UX design, 
+            and front end development. With a strong foundation in HTML/CSS, React, Figma, Python, and SQL, I am equipped to
+             tackle diverse projects and create user-centered experiences. I am passionate about leveraging data to drive
+             insights and decision-making while also focusing on creating intuitive and visually appealing interfaces.
+             With a thirst for knowledge and a drive to excel, I am excited to contribute my skills and make a meaningful
+             impact in the industry.
+             <br/>
+             I'm always happy to connect with new people to share my experiences and also learn from them.
+             </p1>
           </div>
     </div>
   )
